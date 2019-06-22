a113a4bd1bc9553fcf827dc30abcd0f0e6500c73
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-22 14:19:16 +1000

    .

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Sqrt ...
+func Sqrt(x float64) float64 {
+	z := 1.0
+
+	for i := 0; i < 100; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Println(z)
+	}
+
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
